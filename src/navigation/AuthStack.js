3867d0267fc2797825596329b555b313ed03c3d5
Adegoke Temitope <adegoketemitope1909@gmail.com>
2023-01-14 01:16:08 +0100

    configure stack navigator

--- src/navigation/AuthStack.js
+++ src/navigation/AuthStack.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import {View} from 'react-native';
 
+import { createNativeStackNavigator  } from '@react-navigation/native-stack';
+
 import WelcomeScreen from '../screens/WelcomeScreen';
 import Login from '../screens/Login';
 
+const Stack = createNativeStackNavigator();
+
 const AuthStack = () => {
-  return <Login />;
+  return (
+    <Stack.Navigator
+      initialRouteName="Welcome"
+      screenOptions={{
+        headerShown: false,
+      }}>
+      <Stack.Screen name="Welcome" component={WelcomeScreen} />
+      <Stack.Screen name="Login" component={Login} />
+    </Stack.Navigator>
+  );
 };
 
 export default AuthStack;
--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -2,11 +2,14 @@ import React from 'react';
 import {Text, View, Image, Dimensions, TouchableOpacity} from 'react-native';
 import Button from '../components/Button/button';
 import styled from 'styled-components/native';
+import {useNavigation} from '@react-navigation/native';
 
 const DeviceWidth = Dimensions.get('window').width;
 const DeviceHeight = Dimensions.get('window').height;
 
 const WelcomeScreen = () => {
+  const navigator = useNavigation();
+
   return (
     <Container>
       <LogoImage source={require('../assets/images/logo.png')} />
@@ -15,7 +18,10 @@ const WelcomeScreen = () => {
         <SubTitle>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.
         </SubTitle>
-        <Button text="Get Started" />
+        <Button
+          text="Get Started"
+          onPress={() => navigator.navigate('Login')}
+        />
       </ContentWrapper>
     </Container>
   );
@@ -42,7 +48,7 @@ const LogoImage = styled.Image`
 
 const Title = styled.Text`
   font-weight: bold;
-  font-size: ${({theme}) => theme.typeScale.title};
+  font-size: ${({theme}) => theme.typeScale.title}px;
   color: ${({theme}) => theme.blackColor};
   margin-bottom: ${({theme}) => theme.spacing.xs};
 `;
